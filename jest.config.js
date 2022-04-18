c6b83d168c2c2d83cb64cee2e6e3ed3877d4595a
Leonardo Cunha <lmscunha@sga.pucminas.br>
2022-04-18 09:43:41 -0300

    chore: remove esbuild-jest transform - add transformIgnorePatterns

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  transform: {
-    "^.+\\.tsx?$": ["esbuild-jest", {sourcemap:true}]
-  },
-  testEnvironment: 'node',
-  testMatch: ['**/test/**/*.test.ts'],
-  watchPathIgnorePatterns: ['dist\\/'],
-  collectCoverageFrom: ['src/**/*.ts'],
-  coverageProvider: 'v8',
+    testEnvironment: 'node',
+    testMatch: ['**/test/**/*.test.ts'],
+    watchPathIgnorePatterns: ['dist\\/'],
+    collectCoverageFrom: ['src/**/*.ts'],
+    coverageProvider: 'v8',
+    transformIgnorePatterns: [
+        "/node_modules/(?!nordigen-node/lib/|node-fetch/src/|data-uri-to-buffer/dist/|fetch-blob/|formdata-polyfill/)",
+    ],
 };
