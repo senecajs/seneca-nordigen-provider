840d944621cc421aa917d42db4edb33a39f984fa
Leonardo Cunha <lmscunha@sga.pucminas.br>
2022-04-19 05:26:32 -0300

    test: remove promisify conflict

--- test/nordigen-provider.test.ts
+++ test/nordigen-provider.test.ts
@@ -42,7 +42,6 @@ describe('nordigen-provider', () => {
     test('messages', async () => {
         const seneca = Seneca({legacy: false})
             .test()
-            .use('promisify')
             .use('provider', {
                 provider: {
                     nordigen: {
