7c2538bd0126860633c4ae109e39bf4d2483740c
Leonardo Cunha <lmscunha@sga.pucminas.br>
2022-04-21 12:10:24 -0300

    fix: remove unnecessary variable

--- src/cmd-handlers.ts
+++ src/cmd-handlers.ts
@@ -8,8 +8,6 @@ function make_actions(sdk_params: SdkParams, action_details: ActionDetails, sdk:
     async function load(this: any, msg: any) {
         let body = {...msg.q}
 
-        const endpoint_methods: Record<string, any> = sdk.nordigenClient[subpath]
-
         const context = {
             inent: msg.ent,
             request: body,
@@ -20,15 +18,14 @@ function make_actions(sdk_params: SdkParams, action_details: ActionDetails, sdk:
             perform_tasks(before, context)
         }
 
-        const res = await endpoint_methods[cb_name](body)
-
-        let entity: Entity = this.make$(msg.ent.entity$).data$(res.data)
+        const res = await sdk.nordigenClient[subpath][cb_name](body)
+        let entity: Entity = this.make$(msg.ent.entity$).data$(res)
 
         if (after) {
             perform_tasks(after, {
                 ...context,
                 outent: entity,
-                response: res.data
+                response: res
             })
         }
 
@@ -39,5 +36,6 @@ function make_actions(sdk_params: SdkParams, action_details: ActionDetails, sdk:
         load,
     }
 }
+
 
 export {make_actions}
