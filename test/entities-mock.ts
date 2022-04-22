c550019262f11bbc582be10ee958ab718c2cc840
Leonardo Cunha <lmscunha@sga.pucminas.br>
2022-04-22 06:39:05 -0300

    test: [WIP]refactor load institution entities-mock.ts

--- test/entities-mock.ts
+++ test/entities-mock.ts
@@ -1,21 +1,54 @@
 import {TestEntityMap} from "./types-mock"
 
 let test_args = {
-    institution_id: "REVOLUT_REVOGB21",
+    institutionA: {
+        id: "FakeBank123A",
+        name: "Fake Bank A",
+        bic: "ABC1DXXX",
+        transaction_total_days: "500",
+        countries: [
+            "IE"
+        ],
+    },
+    institutionB: {
+        id: "FakeBank123B",
+        name: "Fake Bank B",
+        bic: "ABC2DXXX",
+        transaction_total_days: "537",
+        countries: [
+            "FI"
+        ],
+    },
 }
 
 const entities_tests: TestEntityMap = {
-    institutions: {
+    // institutions: {
+    //     load: {
+    //         args: {
+    //             institution_a: test_args.institutionA,
+    //             institution_b: test_args.institutionB,
+    //         },
+    //         expectations: {
+    //             idA: {
+    //                 sameAs: test_args.institutionA.id,
+    //             },
+    //             idB: {
+    //                 sameAs: test_args.institutionB.id,
+    //             }
+    //         },
+    //     },
+    // },
+    institution: {
         load: {
             args: {
-                institution_id: test_args.institution_id,
+                institution: test_args.institutionA,
             },
             expectations: {
-                id: {
-                    sameAs: test_args.institution_id,
+                institution: {
+                    sameAs: test_args.institutionA,
                 },
             },
-        },
+        }
     },
 }
 
