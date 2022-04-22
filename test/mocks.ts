dd593f4feefc4870bad162532e2dc70f18b26641
Leonardo Cunha <lmscunha@sga.pucminas.br>
2022-04-22 06:38:40 -0300

    test: [WIP]refactor load institution api mock

--- test/mocks.ts
+++ test/mocks.ts
@@ -1,10 +1,44 @@
 export default {
-    institutions: {
+    institution: {
         get: {
             method: "GET",
             url: "/institutions/:id",
             mock_data: {
-                id: 'FakeBank123',
+                institution: {
+                    id: "FakeBank123A",
+                    name: "Fake Bank A",
+                    bic: "ABC1DXXX",
+                    transaction_total_days: "500",
+                    countries: [
+                        "IE"
+                    ],
+                },
+            },
+        },
+    },
+    institutions: {
+        get: {
+            method: "GET",
+            url: "/institutions",
+            mock_data: {
+                institutionA: {
+                    id: "FakeBank123A",
+                    name: "Fake Bank A",
+                    bic: "ABC1DXXX",
+                    transaction_total_days: "500",
+                    countries: [
+                        "IE"
+                    ],
+                },
+                institutionB: {
+                    id: "FakeBank123B",
+                    name: "Fake Bank B",
+                    bic: "ABC2DXXX",
+                    transaction_total_days: "537",
+                    countries: [
+                        "FI"
+                    ],
+                },
             },
         },
     },
