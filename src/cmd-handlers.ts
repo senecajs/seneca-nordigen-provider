db3dfa405c6370051d5469fb93417fcf01d74a72
Leonardo Cunha <lmscunha@sga.pucminas.br>
2022-04-25 05:50:18 -0300

    fix: typo

--- src/cmd-handlers.ts
+++ src/cmd-handlers.ts
@@ -18,14 +18,14 @@ function make_actions(sdk_params: SdkParams, action_details: ActionDetails, sdk:
             perform_tasks(before, context)
         }
 
-        const mockResponse = await sdk.nordigenClient[subpath][cb_name](body)
-        let entity: Entity = this.make$(msg.ent.entity$).data$({res: mockResponse})
+        const ApiResponse = await sdk.nordigenClient[subpath][cb_name](body)
+        let entity: Entity = this.make$(msg.ent.entity$).data$({res: ApiResponse})
 
         if (after) {
             perform_tasks(after, {
                 ...context,
                 outent: entity,
-                response: mockResponse
+                response: ApiResponse
             })
         }
 
