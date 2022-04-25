89ad10ece19082ebd2a73cbe19d48099f2dea096
Leonardo Cunha <lmscunha@sga.pucminas.br>
2022-04-25 09:42:26 -0300

    feat: create list cmd handler for institutions

--- src/cmd-handlers/institution-handlers.ts
+++ src/cmd-handlers/institution-handlers.ts
@@ -5,6 +5,32 @@ function institution_make_actions(sdk_params: SdkParams, action_details: ActionD
     const {subpath} = sdk_params.rest
     const {before, after, cb_name} = action_details
 
+    async function list(this: any, msg: any) {
+        let body = {...msg.q}
+
+        const context = {
+            inent: msg.ent,
+            request: body,
+            query: msg.q,
+        }
+
+        if (before) {
+            perform_tasks(before, context)
+        }
+
+        const apiResponse = await sdk.nordigenClient[subpath][cb_name](body)
+        let entity: Entity = this.make$(msg.ent.entity$).data$({res: apiResponse})
+
+        if (after) {
+            perform_tasks(after, {
+                ...context,
+                outent: entity,
+                response: apiResponse
+            })
+        }
+        return entity
+    }
+
     async function load(this: any, msg: any) {
         let body = {...msg.q}
 
@@ -32,6 +58,7 @@ function institution_make_actions(sdk_params: SdkParams, action_details: ActionD
     }
 
     return {
+        list,
         load,
     }
 }
